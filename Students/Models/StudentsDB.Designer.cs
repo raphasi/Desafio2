c2b4509ea6f7be05754460f111c21ac0ee47ec03
Raphael Andrade <rapha.si@hotmail.com>
2022-03-13 16:00:35 +0000

    Ajustes

--- Students/Models/StudentsDB.Designer.cs
+++ Students/Models/StudentsDB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\_mvc\Students\Students\Models\StudentsDB.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\TFTEC\OneDrive - TFTEC\NoteGuilherme\Html\Students\Students\Models\StudentsDB.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
